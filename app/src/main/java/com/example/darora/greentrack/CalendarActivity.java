df0682b5895f402db2ef9d336037e9822b327b18
Miki <mpokrya@gmail.com>
2016-11-12 14:06:26 -0500

    Added untested parsing code for transaction in Calendar activity.

--- app/src/main/java/com/example/darora/greentrack/CalendarActivity.java
+++ app/src/main/java/com/example/darora/greentrack/CalendarActivity.java
@@ -8,23 +8,33 @@ import com.google.firebase.database.*;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.StringTokenizer;
 
 public class CalendarActivity extends AppCompatActivity {
 
     CalendarView mCalendarView;
     DatabaseReference mRootRef;
+    ArrayList<Transaction> transactionList;
     HashMap<String, ArrayList<Transaction>> dateToTransactionMap;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
         mCalendarView = (CalendarView) findViewById(R.id.calendar);
-
+        transactionList = new ArrayList<>();
         mRootRef.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 for (DataSnapshot childSnapshot : dataSnapshot.getChildren()){
                     Transaction childTrans = childSnapshot.getValue(Transaction.class);
+                    ArrayList<Transaction> updatedTransactionList = dateToTransactionMap.get(
+                            childTrans.getDate());
+                    if (updatedTransactionList == null){
+                        updatedTransactionList = new ArrayList<Transaction>();
+                    }
+                    updatedTransactionList.add(childTrans);
+                    dateToTransactionMap.put(childTrans.getDate(), updatedTransactionList);
+                    transactionList.add(childTrans);
                 }
             }
 
