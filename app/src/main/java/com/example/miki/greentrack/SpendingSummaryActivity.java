731a0a74bc96e17584be28648308b6e1bc240a32
Miki <mpokrya@gmail.com>
2016-11-12 09:06:04 -0500

    Added basic CalendarView

---
+++ app/src/main/java/com/example/miki/greentrack/SpendingSummaryActivity.java
@@ -0,0 +1,13 @@
+package com.example.miki.greentrack;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SpendingSummaryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_spending_summary);
+    }
+}
