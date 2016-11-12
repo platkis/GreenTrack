d6c670e44f9c6f136fe1deea322d69b8b5c34df2
Divjot Arora <divjot.arora@gmail.com>
2016-11-12 16:42:21 -0500

    transactions

--- app/src/main/java/com/example/darora/greentrack/Limits.java
+++ app/src/main/java/com/example/darora/greentrack/Limits.java
@@ -5,97 +5,18 @@ package com.example.darora.greentrack;
  */
 
 public class Limits {
-    //    Financial - DELETE - DONE
-    //
-    //    Home improvement - DELETE
-    //    Rental Car & Taxi - DELETE
-    //    Service & Parts - DELETE
-    //            Vacation - DELETE
-    //    Check - DELETE
-
-//    Uncategorized - CHANGE MISC
-
-//    Credit Card payment
-//            Groceries
-//    Coffee Shops
-//            Clothing
-//    Electronics & Software
-//            Shopping
-//    Cash & ATM
-
-//    Pharmacy - CHANGE TO MEDICAL
-//    Doctor - CHANGE TO MEDICAL
-
-//    Food & Dining - Fast food + food & dining + restaurants
-            //  Restaurants - CHANGE TO FOOD
-            //    Fast Food - CHANGE TO FOOD
-
-    private double misc;
-    private double foodDining;
-    private double creditCard;
-    private double groceries;
-    private double coffee;
-    private double medical;
-    private double clothing;
-    private double electronics;
-    private double shopping;
-    private double cashAtm;
-
-    public Limits(double misc, double foodDining, double creditCard, double groceries, double coffee,
-                  double medical, double clothing, double electronics, double shopping, double cashAtm) {
-        this.misc = misc;
-        this.foodDining = foodDining;
-        this.creditCard = creditCard;
-        this.groceries = groceries;
-        this.coffee = coffee;
-        this.medical = medical;
-        this.clothing = clothing;
-        this.electronics = electronics;
-        this.shopping = shopping;
-        this.cashAtm = cashAtm;
-    }
+    public double miscellaneous;
+    public double food;
+    public double creditCard;
+    public double groceries;
+    public double coffee;
+    public double medical;
+    public double clothing;
+    public double electronics;
+    public double shopping;
+    public double cashAtm;
 
     public Limits() {
 
-    }
-
-    public double getCashAtm() {
-        return cashAtm;
-    }
-
-    public double getMisc() {
-        return misc;
-    }
-
-    public double getFoodDining() {
-        return foodDining;
-    }
-
-    public double getCreditCard() {
-        return creditCard;
-    }
-
-    public double getGroceries() {
-        return groceries;
-    }
-
-    public double getCoffee() {
-        return coffee;
-    }
-
-    public double getMedical() {
-        return medical;
-    }
-
-    public double getClothing() {
-        return clothing;
-    }
-
-    public double getElectronics() {
-        return electronics;
-    }
-
-    public double getShopping() {
-        return shopping;
     }
 }
