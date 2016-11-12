8396e996a116b8a7ca00185560c7f5ace261660b
Miki <mpokrya@gmail.com>
2016-11-12 13:37:05 -0500

    Added some Firebase integration to CalendarActivity.

--- app/src/main/java/com/example/darora/greentrack/CalendarActivity.java
+++ app/src/main/java/com/example/darora/greentrack/CalendarActivity.java
@@ -4,14 +4,34 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.CalendarView;
 
+import com.google.firebase.database.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class CalendarActivity extends AppCompatActivity {
 
     CalendarView mCalendarView;
-
+    DatabaseReference mRootRef;
+    HashMap<String, ArrayList<Transaction>> dateToTransactionMap;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
         mCalendarView = (CalendarView) findViewById(R.id.calendar);
+
+        mRootRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot childSnapshot : dataSnapshot.getChildren()){
+                    Transaction childTrans = childSnapshot.getValue(Transaction.class);
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
     }
 }
