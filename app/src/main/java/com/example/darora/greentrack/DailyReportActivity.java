13b976e39598489e84249a4d146aa731444f76d8
Miki <mpokrya@gmail.com>
2016-11-12 22:40:29 -0500

    Properly aligned (not properly vertically spaced)

--- app/src/main/java/com/example/darora/greentrack/DailyReportActivity.java
+++ app/src/main/java/com/example/darora/greentrack/DailyReportActivity.java
@@ -5,6 +5,9 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutCompat;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.GridLayout;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextClock;
@@ -18,26 +21,67 @@ import java.util.ArrayList;
 
 public class DailyReportActivity extends AppCompatActivity {
 
-    LinearLayout mLayout;
+    GridLayout mLayout;
+    int textSize;
 
     @Override
-    protected void onCreate(Bundle onSavedInstanceState){
+    protected void onCreate(Bundle onSavedInstanceState) {
         super.onCreate(onSavedInstanceState);
         setContentView(R.layout.acitivity_daily_report);
-        mLayout =(LinearLayout) findViewById(R.id.daily_report_layout);
+        mLayout = (GridLayout) findViewById(R.id.daily_report_layout);
+        textSize = 20;
         Intent receivedIntent = getIntent();
         Bundle receivedBundle = receivedIntent.getExtras();
         ArrayList<Transaction> transactionList = (ArrayList<Transaction>) receivedBundle.getSerializable(
                 "transactionList");
         if (transactionList != null) {
+            //mLayout.set
             for (int i = 0; i < transactionList.size(); i++) {
-                TextView txtView = new TextView(DailyReportActivity.this);
-                txtView.setText(transactionList.get(i).getDate() + "\t" +
-                        transactionList.get(i).getName() + "\t" + transactionList.get(i).getAmount());
-                txtView.setTextSize(15);
-                txtView.setPadding(0, (i * 30), 0, 0);
+                TextView nameTV = new TextView(DailyReportActivity.this);
+                String nameText = transactionList.get(i).getName();
+              //  nameTV.setGravity(Gravity.START);
+                nameTV.setText(nameText);
+                nameTV.setTextSize(textSize);
+                //nameTV.setTextAlignment(View.TEXT_ALIGNMENT_TEXT_START);
                 mLayout.setBackgroundColor(Color.TRANSPARENT);
-                mLayout.addView(txtView);
+                /**
+                GridLayout.LayoutParams param1 =new GridLayout.LayoutParams();
+                param1.height = GridLayout.LayoutParams.WRAP_CONTENT;
+                param1.width = GridLayout.LayoutParams.MATCH_PARENT;
+                param1.leftMargin = 10;
+                param1.setGravity(Gravity.START | Gravity.FILL);
+                nameTV.setLayoutParams(param1);
+                 **/
+                GridLayout.Spec titleTxtSpecColumn1 = GridLayout.spec(1, GridLayout.LEFT);
+                GridLayout.Spec titleRowSpec1 = GridLayout.spec(i);
+                mLayout.addView(nameTV, new GridLayout.LayoutParams(titleRowSpec1 , titleTxtSpecColumn1));
+
+
+
+
+
+                TextView amountTV = new TextView(DailyReportActivity.this);
+                String amountText = "" + transactionList.get(i).getAmount();
+                //amountTV.setTextAlignment(View.TEXT_ALIGNMENT_TEXT_END);
+                //amountTV.setGravity(Gravity.END);
+                amountTV.setText(amountText);
+                amountTV.setTextSize(textSize);
+                /**
+                GridLayout.LayoutParams param2 =new GridLayout.LayoutParams();
+                param2.height = GridLayout.LayoutParams.WRAP_CONTENT;
+                param2.width = GridLayout.LayoutParams.MATCH_PARENT;
+                param2.rightMargin = 20;
+                param2.setGravity(Gravity.END | Gravity.FILL);
+                amountTV.setLayoutParams(param2);
+                //param.topMargin = 5;
+                 **/
+
+
+                GridLayout.Spec titleTxtSpecColumn2 = GridLayout.spec(2, GridLayout.RIGHT);
+                GridLayout.Spec titleRowSpec2 = GridLayout.spec(i);
+                mLayout.addView(amountTV, new GridLayout.LayoutParams(titleRowSpec2 , titleTxtSpecColumn2));
+                mLayout.setBackgroundColor(Color.TRANSPARENT);
+
             }
         }
     }
