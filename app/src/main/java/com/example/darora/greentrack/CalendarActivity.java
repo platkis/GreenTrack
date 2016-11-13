0dc8ba11bfd7b1d9df04c1c57927123d449d51e6
Miki <mpokrya@gmail.com>
2016-11-13 00:54:44 -0500

    Push notification starts activity.

--- app/src/main/java/com/example/darora/greentrack/CalendarActivity.java
+++ app/src/main/java/com/example/darora/greentrack/CalendarActivity.java
@@ -1,6 +1,12 @@
 package com.example.darora.greentrack;
 
+import android.app.AlarmManager;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
+import android.icu.util.Calendar;
 import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -26,6 +32,8 @@ public class CalendarActivity extends AppCompatActivity {
     DrawerLayout mDrawerLayout;
     ActionBarDrawerToggle mDrawerToggle;
     Button mSetLimitsButton;
+    private AlarmManager alarmMgr;
+    private PendingIntent alarmIntent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,6 +46,7 @@ public class CalendarActivity extends AppCompatActivity {
         dateToTransactionMap = new HashMap<>();
         mRootRef = FirebaseDatabase.getInstance().getReference();
 
+        pushNotification("HEY BITCH");
 
         NavigationView mDrawerList = (NavigationView) findViewById(R.id.main_navigation);
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
@@ -96,6 +105,35 @@ public class CalendarActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    public void pushNotification(String notificationText) {
+        Context context = getApplicationContext();
+        NotificationManager notificationManager = (NotificationManager) context
+                .getSystemService(Context.NOTIFICATION_SERVICE);
+        Intent notificationIntent = new Intent(CalendarActivity.this, LimitReportActivity.class);
+        PendingIntent pending = PendingIntent.getActivity(getApplicationContext(), 0, notificationIntent, 0);
+
+
+        Notification.Builder builder = new Notification.Builder(getApplicationContext());
+        builder.setContentText(notificationText);
+        builder.setSmallIcon(R.drawable.ic_stat_name);
+        Notification notification = builder.build();
+        notificationManager.notify(0, notification);
+        builder.setContentIntent(pending);
+
+
+        notificationIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP
+                | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+
+        //PendingIntent intent = PendingIntent.getActivity(context, 0,
+          //      notificationIntent, 0);
+
+
+       // notification.setLatestEventInfo(context, "This is the title", "This is message", intent);
+
+        notification.flags |= Notification.FLAG_AUTO_CANCEL;
+        notificationManager.notify(0, notification);
     }
     @Override
     protected void onStart(){
---
+++ app/src/main/java/com/example/darora/greentrack/LimitReportActivity.java
@@ -0,0 +1,18 @@
+package com.example.darora.greentrack;
+
+import android.app.Application;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Miki on 11/13/2016.
+ */
+
+public class LimitReportActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle onSavedInstanceState){
+        super.onCreate(onSavedInstanceState);
+        setContentView(R.layout.acitivity_daily_report);
+    }
+}
